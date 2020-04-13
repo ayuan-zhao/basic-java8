24499d1a978de82bc32da305c61aa911d1aa6a58
Qiqiang Guan <qiqguan@paypal.com>
2020-04-12 20:33:15 -0700

    add some practice loop

---
+++ basic-java8-core/src/main/java/edu/gqq/ayuan/NumberReverse.java
@@ -0,0 +1,13 @@
+package edu.gqq.ayuan;
+
+public class NumberReverse {
+    public static void main(String[] args) {
+        int num = 12345;
+        String res = "";
+        while (num > 0) {
+            res += num % 10;
+            num /= 10;
+        }
+        System.out.println(res);
+    }
+}
